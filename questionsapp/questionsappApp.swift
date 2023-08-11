322275b8fbb6fa7455f0cdcbfd5a6274983bc3a4
Celina <celinayau28@gmail.com>
2023-08-11 12:53:06 -0400

    Initial Commit

---
+++ questionsapp/questionsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionsappApp.swift
+//  questionsapp
+//
+//  Created by scholar on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct questionsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
